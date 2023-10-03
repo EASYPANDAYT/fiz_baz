d0d7735e40ee8254981d762728ee23768ac2ccb2
EASYPANDAYT <145465554+EASYPANDAYT@users.noreply.github.com>
2023-10-03 17:41:25 +0300

    Add files via upload

---
+++ упуп.py
@@ -0,0 +1,11 @@
+number = 0
+while number <101:
+    if number % 3 and % 5 == 0:
+        print ('физбаз')
+    elif number % 3 == 0 :
+        print ('физ')
+    elif number % 5 == 0 :
+        print ('баз')
+    else :
+        print (number)
+    number += 1
